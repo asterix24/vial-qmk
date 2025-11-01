7fba0ca177516f9debeaf5be32c8e86d48255a08
Daniele Basile <asterix@develer.com>
2025-11-02 00:49:23 +0100

    [isokey] Fix left matrix settings

--- keyboards/isokey/v5_1/keymaps/default/keymap.c
+++ keyboards/isokey/v5_1/keymaps/default/keymap.c
@@ -9,11 +9,11 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DB] = LAYOUT_ortho_5x7_5x9 (
-        KC_B,KC_A,KC_B,KC_A,KC_B,KC_A,KC_B,
-             KC_A,KC_B,KC_A,KC_B,KC_A,KC_B,
-             KC_A,KC_B,KC_A,KC_B,KC_A,KC_B,
-        KC_B,KC_A,KC_B,KC_A,KC_B,KC_A,KC_B,
-        KC_B,KC_A,KC_B,     KC_B,KC_A,
+        KC_5,KC_A,KC_B,KC_A,KC_B,KC_A,KC_3,
+             KC_0,KC_B,KC_A,KC_B,KC_A,KC_9,
+             KC_0,KC_B,KC_A,KC_B,KC_A,KC_9,
+        KC_5,KC_A,KC_B,KC_A,KC_B,KC_A,KC_3,
+        KC_5,KC_A,KC_B,     KC_B,KC_2,
 // - ------------------------------------------------
         KC_D,KC_H,KC_A,KC_D,KC_X,KC_Y,KC_D,KC_A,
         KC_D,KC_H,KC_A,KC_D,KC_X,KC_Y,KC_D,     KC_H,
