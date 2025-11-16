accf9a0aef2276f09bf4fcfb0e1ef350bc1f008f
Daniele Basile <asterix@develer.com>
2025-11-16 01:08:14 +0100

    [isokey] Fix vial configuration. Fix num pad layer

--- keyboards/isokey/v5_1/keymaps/vial/keymap.c
+++ keyboards/isokey/v5_1/keymaps/vial/keymap.c
@@ -4,10 +4,10 @@
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-    _DB,
     _BASE,
     _FN,
     _NUM,
+    _DB,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -27,19 +27,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
 
     [_BASE] = LAYOUT_ortho_5x7_5x9 (
-        KC_ESC, KC_GRV , KC_1,    KC_2,    KC_3,   KC_4,   KC_5,
-                KC_TAB , KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,
-                MO(_FN), KC_A,    KC_S,    KC_D,   KC_F,   KC_G,
-        MO(_FN) KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
-        MO(_FN),KC_LCTL, KC_LGUI,          KC_LALT,KC_SPC,
+        KC_ESC,    KC_GRV ,  KC_1,    KC_2,    KC_3,     KC_4,   KC_5,
+                   KC_TAB ,  KC_Q,    KC_W,    KC_E,     KC_R,   KC_T,
+                   MO(_FN),  KC_A,    KC_S,    KC_D,     KC_F,   KC_G,
+        MO(_FN),   KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,   KC_B,
+        MO(_NUM),  KC_LCTL,  KC_LGUI,          KC_LALT,  KC_SPC,
 
 // - -----------------------------------------------------------------------------------//
 
-        KC_6,   KC_7,    KC_8   , KC_9   ,KC_0   , KC_MINS,KC_EQL , KC_BSPC,
-        KC_Y,   KC_U,    KC_I   , KC_O   ,KC_P   , KC_LBRC,KC_RBRC, KC_ENT,  KC_INS
-        KC_H,   KC_J,    KC_K   , KC_L   ,KC_SCLN, KC_QUOT,KC_BSLS,          KC_DEL
-        KC_N,   KC_M,    KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT          KC_UP,
-                KC_SPC,  KC_ALGR,         KC_RCTL,         KC_LEFT, KC_DOWN, KC_RGHT
+        KC_6,   KC_7,    KC_8   , KC_9   ,KC_0   , KC_MINS, KC_EQL , KC_BSPC,
+        KC_Y,   KC_U,    KC_I   , KC_O   ,KC_P   , KC_LBRC, KC_RBRC,          KC_INS,
+        KC_H,   KC_J,    KC_K   , KC_L   ,KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,  KC_DEL,
+        KC_N,   KC_M,    KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,          KC_UP,
+                KC_SPC,  KC_ALGR,         KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
     ),
 //      _______,_______,_______,_______,_______,_______,_______,
 //      _______ _______,_______,_______,_______,_______,_______,
@@ -55,8 +55,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_FN] = LAYOUT_ortho_5x7_5x9 (
       _______,_______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5,
-      _______ _______,_______,_______,_______,_______,_______,
-      _______ _______,_______,_______,_______,_______,_______,
+              _______,_______,_______,_______,_______,_______,
+              _______,_______,_______,_______,_______,_______,
       _______,_______,_______,_______,_______,_______,_______,
       _______,_______,_______,        _______,_______,
 // - -----------------------------------------------------------------------------------//
@@ -65,6 +65,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______,KC_LEFT,KC_DOWN, KC_UP,KC_RIGHT,_______,_______,_______,_______,
       _______,_______,_______,_______,_______,_______,        KC_PGUP,
               _______,_______,        _______,        KC_HOME,KC_PGDN,KC_END
+    ),
+
+    [_NUM] = LAYOUT_ortho_5x7_5x9 (
+
+      _______,_______,_______,_______,_______,_______,_______,
+              _______,_______,_______,_______,_______,_______,
+              _______,_______,_______,_______,_______,_______,
+      _______,_______,_______,_______,_______,_______,_______,
+      _______,_______,_______,        _______,_______,
+// - -----------------------------------------------------------------------------------//
+      _______,KC_PSLS,KC_PAST, KC_PMNS,_______,_______,_______,_______,
+      KC_P7,  KC_P8,   KC_P9,  KC_PPLS,_______,_______,_______,        _______,
+      KC_P4,  KC_P5,   KC_P6,  KC_PPLS,_______,_______,_______,_______,_______,
+      KC_P1,  KC_P2,   KC_P3,  KC_PENT,_______,_______,        _______,
+              KC_P0,   KC_PDOT,        KC_COMM,        _______,_______,_______
     ),
 
     [_DB] = LAYOUT_ortho_5x7_5x9 (
